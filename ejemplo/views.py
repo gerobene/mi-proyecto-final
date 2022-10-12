4f0ab371d7639f2a5d54c9c9b7a3eee1e4205ff6
Geronimo Benedetti <geronimobenedetti@MacBook-Pro-de-Geronimo.local>
2022-10-11 23:26:48 -0300

    agreggo ejemplos de clase 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Gero"}, {"apellido":"Benedetti"})
     
-# Create your views here.
